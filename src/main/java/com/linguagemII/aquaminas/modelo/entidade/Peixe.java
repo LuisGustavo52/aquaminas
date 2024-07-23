85c2aebeaa2c9c0459faf1e34b5bfc7b7f5ff74d
p3drobitencourt <pedro.b1tencourt2801@gmail.com>
2024-07-23 20:24:23 -0300

    Criação de Entidades

--- src/main/java/com/linguagemII/aquaminas/modelo/entidade/Peixe.java
+++ src/main/java/com/linguagemII/aquaminas/modelo/entidade/Peixe.java
@@ -9,5 +9,41 @@ package com.linguagemII.aquaminas.modelo.entidade;
  * @author 13306819670
  */
 public class Peixe {
+    private int idPeixe;
+    private int valor_Unid;
+    private int especie;
+    private char nome_cientifico;
+
+    public int getIdPeixe() {
+        return idPeixe;
+    }
+
+    public void setIdPeixe(int idPeixe) {
+        this.idPeixe = idPeixe;
+    }
+
+    public int getValor_Unid() {
+        return valor_Unid;
+    }
+
+    public void setValor_Unid(int valor_Unid) {
+        this.valor_Unid = valor_Unid;
+    }
+
+    public int getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(int especie) {
+        this.especie = especie;
+    }
+
+    public char getNome_cientifico() {
+        return nome_cientifico;
+    }
+
+    public void setNome_cientifico(char nome_cientifico) {
+        this.nome_cientifico = nome_cientifico;
+    }
     
 }
---
+++ src/main/java/com/linguagemII/aquaminas/modelo/entidade/Venda_Peixe.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.linguagemII.aquaminas.modelo.entidade;
+
+/**
+ *
+ * @author 17933118623
+ */
+public class Venda_Peixe {
+    private Integer quantidade_Peixe;
+    private Venda venda;
+    private peixe peixe;
+
+    public Integer getQuantidade_Peixe() {
+        return quantidade_Peixe;
+    }
+
+    public void setQuantidade_Peixe(Integer quantidade_Peixe) {
+        this.quantidade_Peixe = quantidade_Peixe;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+
+    public peixe getPeixe() {
+        return peixe;
+    }
+
+    public void setPeixe(peixe peixe) {
+        this.peixe = peixe;
+    }
+}
