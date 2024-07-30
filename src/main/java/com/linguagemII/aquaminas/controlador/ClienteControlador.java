2529d0700d8b77abc3bcc8f4d70db50449aa28cc
p3drobitencourt <pedro.b1tencourt2801@gmail.com>
2024-07-29 22:43:35 -0300

    alterar

--- src/main/java/com/linguagemII/aquaminas/controlador/ClienteControlador.java
+++ src/main/java/com/linguagemII/aquaminas/controlador/ClienteControlador.java
@@ -4,8 +4,8 @@
  */
 package com.linguagemII.aquaminas.controlador;
 
-import com.linguagemII.aquaminas.modelo.dao.FuncaoDao;
-import com.linguagemII.aquaminas.modelo.entidade.Funcao;
+import com.linguagemII.aquaminas.modelo.dao.ClienteDao;
+import com.linguagemII.aquaminas.modelo.entidade.Cliente;
 import com.linguagemII.aquaminas.servico.WebConstantes;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -20,36 +20,40 @@ import java.util.List;
  *
  * @author 17933118623
  */
-
     /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-@WebServlet(WebConstantes.BASE_PATH + "/FuncaoControlador")
+@WebServlet(WebConstantes.BASE_PATH + "/ClienteControlador")
 
 
-public class FuncaoControlador extends HttpServlet {
+public class ClienteControlador extends HttpServlet {
 
-    private FuncaoDao FuncaoDao;
-    private Funcao Funcao;
-    String idFuncao = "";
-    String funcao = "";
+    private ClienteDao clienteDao;
+    private Cliente cliente;
+    String idCliente = "";
+    String cpf = "";
+    String telefone = "";
+    String nome = "";
     String opcao = "";
 
     @Override
     public void init() throws ServletException {
-        FuncaoDao = new FuncaoDao();
-        Funcao = new Funcao();
+        clienteDao = new ClienteDao();
+        cliente = new Cliente();
     }
-    
     
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             opcao = request.getParameter("opcao");
-            idFuncao = request.getParameter("idFuncao");
-            funcao = request.getParameter("funcao");
+            idCliente = request.getParameter("idCliente");
+            cliente = request.getParameter("Cliente");
+            cpf = request.getParameter("cpf");
+            telefone = request.getParameter("telefone");
+            nome = request.getParameter("nome");
+            
             if (opcao == null || opcao.isEmpty()) {
                 opcao = "cadastrar";
             }
@@ -74,60 +78,61 @@ public class FuncaoControlador extends HttpServlet {
 
     private void cadastrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         validaCampos();
-        Funcao.setIdFuncao (Integer.parseInt(idFuncao));
-        Funcao.setFuncao(funcao);
-        FuncaoDao.salvar(Funcao);
+        cliente.setIdCliente(Integer.parseInt(idCliente));
+        cliente.setCliente(idCliente);
+        clienteDao.salvar(Cliente);
+        
         
         encaminharParaPagina(request, response);
     }
 
     private void editar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("idFuncao", idFuncao);
+        request.setAttribute("idCliente", idCliente);
         request.setAttribute("opcao", "confirmarEditar");
-        request.setAttribute("Funcao", Funcao);
+        request.setAttribute("Cliente", cliente);
         request.setAttribute("mensagem", "Edite os dados e clique em salvar");
         encaminharParaPagina(request, response);
     }
     private void excluir(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("idFuncao", idFuncao);
+        request.setAttribute("idCliente", idCliente);
         request.setAttribute("opcao", "confirmarEditar");
-        request.setAttribute("Funcao", Funcao);
+        request.setAttribute("Cliente", cliente);
         request.setAttribute("mensagem", "Edite os dados e clique em salvar");
         encaminharParaPagina(request, response);
     }
 
     private void confirmarEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         validaCampos();
-        Funcao.setIdFuncao(Integer.valueOf(idFuncao));
-        Funcao.setFuncao(Funcao);
-        FuncaoDao.alterar(Funcao);
+        Cliente.setIdCliente(Integer.valueOf(idCliente));
+        Cliente.setCliente(cliente);
+        ClienteDao.alterar(cliente);
         cancelar(request, response);
     }
     private void confirmarExcluir(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Funcao.setIdFuncao(Integer.valueOf(idFuncao));
-        Funcao.setFuncao(Funcao);
-        FuncaoDao.excluir(Funcao);
+        Cliente.setIdCliente(Integer.valueOf(idCliente));
+        Cliente.setCliente(cliente);
+        ClienteDao.excluir(cliente);
         cancelar(request, response);
     }
 
     private void cancelar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("IdFuncao", "0");
+        request.setAttribute("IdCliente", "0");
         request.setAttribute("opcao", "cadastrar");
-        request.setAttribute("Funcao", "");
+        request.setAttribute("Cliente", "");
         encaminharParaPagina(request, response);
     }
 
     private void encaminharParaPagina(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Funcao> Funcaos = FuncaoDao.buscarTodas();
-        request.setAttribute("Funcaos", Funcaos);
+        List<Cliente> Clientes = ClienteDao.buscarTodas();
+        request.setAttribute("Clientes", Clientes);
         request.setAttribute(opcao, opcao);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/CadastroFuncao.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/CadastroCliente.jsp");
         dispatcher.forward(request, response);
 
     }
     
     public void validaCampos(){
-        if(Funcao==null || Funcao.isEmpty() ){
+        if(Cliente==null || Cliente.isEmpty() ){
             throw new IllegalArgumentException("Um ou mais parâmetros estão ausentes");
         }
     }
